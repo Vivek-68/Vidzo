377c79ee6a6ef293e84f0638db5de3e3da80c75e
Vivek-68 <vivektitan3@gmail.com>
2024-04-07 02:20:58 +0530

    ADD: user and video models

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js';
 dotenv.config({
     path:'./env'
 })
-connectDB();
+connectDB().then(
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server running at port ${process.env.PORT}`);
+    })
+)
+.catch(err => console.error("Database connection failed!",err));
